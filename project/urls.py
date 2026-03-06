9f819b38394892e0468045ce6d04d592ffeff947
Pulkit Kumar <pulkitindia5@gmail.com>
2026-03-06 19:31:00 +0530

    Feature: Add Staff Management tab and backend views for doctor account creation. Fix MediVoice config loading.

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import login_view,patient_insert_view,CallFeedbackView,EscalationfeedbackView,UpdateEscalation,CommunityfeedbackView,UpdateCommunity,EscalationManagementView,fetchpatients,fetchrecentactivity,AdminDashboardView,KPISummary,Patientengagement,CommunityEngagement,EscalationEngagement,validateToken,upload_files_log,Allhospitals,RevisitAnalyticsAPIView,TextView,tab_access,PdfView,CampaignView,ROIMetrics,DepartmentAnalytics,doctor_login_view,MediVoiceSessionView,DoctorTranscriptionView
+from app.views import (
+    login_view, patient_insert_view, CallFeedbackView, EscalationfeedbackView, 
+    UpdateEscalation, CommunityfeedbackView, UpdateCommunity, EscalationManagementView, 
+    fetchpatients, fetchrecentactivity, AdminDashboardView, KPISummary, 
+    Patientengagement, CommunityEngagement, EscalationEngagement, validateToken, 
+    upload_files_log, Allhospitals, RevisitAnalyticsAPIView, TextView, 
+    tab_access, PdfView, CampaignView, ROIMetrics, DepartmentAnalytics, 
+    doctor_login_view, MediVoiceSessionView, DoctorTranscriptionView, 
+    DoctorManagementView
+)
 from phone_calling.views import Outbound_call,Process_Outbound_call,download_excel_outbound,Inboundcall,showInboundcall,processinboundcall_view
 from inbound_dashboard.views import Patientengagement_inbound,CallFeedbackView_inbound,CommunityfeedbackView_inbound,CommunityEngagement_inbound,EscalationfeedbackView_inbound,EscalationEngagement_inbound,RevisitAnalyticsAPIView_inbound,KPISummary_inbound,UpdateEscalation_inbound
+
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/login/',login_view.as_view()),
@@ -44,6 +54,7 @@ urlpatterns = [
     path("api/tab_access/",tab_access.as_view()),
     path("api/pdf_view/",PdfView.as_view()),
     path("api/campaigns/",CampaignView.as_view()),
+    path("api/doctor_management/", DoctorManagementView.as_view()),
     path("api/doctor/login/", doctor_login_view.as_view()),
     path("api/medivoice/sessions/", MediVoiceSessionView.as_view()),
     path("api/hospital/doctor_transcripts/", DoctorTranscriptionView.as_view()),
@@ -64,6 +75,4 @@ urlpatterns = [
     path("api/kpisummary_inbound/",KPISummary_inbound.as_view()),
     path("api/update_escalation_inbound/",UpdateEscalation_inbound.as_view()),
     path("api/process_inbound_call/",processinboundcall_view.as_view()),
-    
-    
 ]

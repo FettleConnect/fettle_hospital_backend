1bf909c25c251d02ee00e3bddb9e2687211df62b
Sushant Shah <sushantshah@cyrocoders.dev>
2026-03-17 00:46:06 +0000

    Add RUN_MIGRATIONS environment variable to avoid migration race conditions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
-echo ">>> Running database migrations..."
-python manage.py migrate --noinput
+RUN_MIGRATIONS_NORMALIZED=$(printf '%s' "${RUN_MIGRATIONS:-true}" | tr '[:upper:]' '[:lower:]')
+if [ "$RUN_MIGRATIONS_NORMALIZED" = "true" ]; then
+  echo ">>> Running database migrations..."
+  python manage.py migrate --noinput
+else
+  echo ">>> Skipping database migrations (RUN_MIGRATIONS=${RUN_MIGRATIONS:-false})"
+fi
 
 # Collect static files in production (DEBUG=False)
 DEBUG_NORMALIZED=$(printf '%s' "$DEBUG" | tr '[:upper:]' '[:lower:]')
